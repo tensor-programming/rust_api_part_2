604b421a8faf87b15e8f0b13f50e1eea7d586f27
Tensor-Programming <abeltensor@tensor-programming.com>
2018-05-01 01:46:30 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@
 
 #[macro_use]
 extern crate diesel;
-#[macro_use]
-extern crate diesel_codegen;
+//no longer needed
+//#[macro_use]
+//extern crate diesel_codegen;
 extern crate dotenv;
 extern crate r2d2;
 extern crate r2d2_diesel;
